55c6f2ae35265d169774feb8eeae2e2fb86df39e
luishdz4757 <53283167+luishdz4757@users.noreply.github.com>
2019-10-22 16:04:51 -0700

    Update app.js

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -4,29 +4,26 @@
 
 
 class Game {
-    //btn id btn_reset - add event listener
-   // btn class keyrow - add event listener
    constructor () {
     this.missed = 0;
-    this.phrase = [] ;
-    this.activePhrase = 'null';
+    this.phrase = this.createPhrases();
+    this.activePhrase = null;
    }
-   createPhrases () {
-       new Phrase ('life is like a box of chocolates'),
+   createPhrases(){
+       const phrases = [
+       new Phrase ('life is like a box of chocolates'), 
        new Phrase ('there is no trying'),
        new Phrase ('may the force be with you'),
        new Phrase ('you have to see the matrix for yourself'),
-       new Phrase ('you talking to me')
-       phrases.push(createPhrases);
+       new Phrase ('you talking to me')];
+       return phrases;
    }
+  // getRandomPhrase(){
+   //    const randomNum = Math.floor(Math.random() * this.phrase.legnth)
+  // }
 
  }
-
-const phrase = new Phrase();
-//console.log(`Phrase - phrase: ${phrase.phrase}`);
-
-const game = new Game();
-
-game.phrases.forEach((phrase, index) => {
-    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+ const game = new Game();
+ game.phrases.forEach((phrase, index) => {
+ console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
 });
