8df78cb6838764fa891449c3e170da8688076cb4
luishdz4757 <53283167+luishdz4757@users.noreply.github.com>
2019-10-24 19:49:01 -0700

    Update Game.js

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -2,10 +2,38 @@
  * Project 4 - OOP Game App
  * Game.js */
 
-//  class Game {
-//     constructor () {
-//         this.missed = 0;
-//         this.phrase = '';
-//         this.activePhrase = 'null';
-//     }
-//  }
+class Game {
+    constructor () {
+     this.missed = 0;
+     this.phrase = this.createPhrases();
+     this.activePhrase = this.getRandomPhrase();
+    }
+    createPhrases(){
+        const phrases = 
+        ('life is like a box of chocolates', 
+        'there is no trying',
+        'may the force be with you',
+        'you have to see the matrix for yourself',
+        'you talking to me');
+        return phrases;
+    }
+    getRandomPhrase(){
+        const randomNum = Math.floor(Math.random() * this.phrase.length)
+        return this.phrase[randomNum];
+    }
+  }
+  const game = new Game();
+ game.phrases.forEach((phrase, index) => {
+ console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+ });
+ //  const logPhrase = (phrase) => {
+ //     console.log(`Phrase - phrase: `, phrase.phrase);
+ //    };
+ //     const game = new Game();
+     
+ //     logPhrase(game.getRandomPhrase());
+ //     logPhrase(game.getRandomPhrase());
+ //     logPhrase(game.getRandomPhrase());
+ //     logPhrase(game.getRandomPhrase());
+ //     logPhrase(game.getRandomPhrase());
+ 
