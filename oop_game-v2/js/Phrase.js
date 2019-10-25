81ea0b22a68ffed567da425a0f592de720acac42
luishdz4757 <53283167+luishdz4757@users.noreply.github.com>
2019-10-24 21:22:56 -0700

    Update Phrase.js

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -4,6 +4,24 @@
 
  class Phrase {
      constructor (phrase) {
-        phrase = phrase
+        this.phrase = phrase.toLowerCase();
+     }
+     addPhraseToDisplay() {
+         //Targets the phrase
+        const phraseElement = document.querySelector('#phrase ul');
+        const splitPhrase = phrase.split('');
+        splitPhrase.forEach(function (character) {
+            const liElementChar = document.createElement('li');
+            phraseElement.append(LiElementChar);
+            liElementChar.classList.add('hide');
+            if (character === ' ') {
+                liElementChar.classList.add('space');
+            } else {
+                liElementChar.classList.add('letter');
+            }
+        });
      }
  }
+
+ const game = new Game();
+ game.getRandomPhrase().addPhraseToDisplay();
